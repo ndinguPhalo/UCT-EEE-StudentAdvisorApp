6b990f8b5c6f472e2f93b8909e34ac649b555e3a
Retrospect <mathunganap@gmail.com>
2022-06-23 14:30:10 +0200

    Had to change from using DictReader to regular reader and it has caused plenty of pain but now I can move forward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from curses import def_shell_mode
-from re import T
+#from curses import def_shell_mode
+#from re import T
 import tkinter as tk
 from tkinter import filedialog
 import csv
@@ -25,6 +25,8 @@ stdarr = []
 carr = []
 # Array required for finding out lengths of individual student records
 yi = []
+# Array for storing latest year on transript info
+cyearArr = []
 
 # Required for creating the Dialog Box
 root = tk.Tk()
@@ -37,6 +39,7 @@ with open(file_path, mode ='r') as file:
        csvFile = csv.DictReader(file)
        tl = 0
        fc = 0
+       cyear = 0
        for lines in csvFile:
         line = json.dumps(lines)
         x = line.rstrip().split(",")
@@ -44,14 +47,36 @@ with open(file_path, mode ='r') as file:
             if (fc != 0):
                 if (tl != 0):
                     yi.append(tl)
+                    cyearArr.append(pyear)
+                    cyear = 0
+                    pyear = 0
                     tl = 1
                 else:
                     tl = 1
+                    cyear = 0
+                    pyear = 0
             else:
                 fc = 1
         elif (tl > 0):
+            ns = x[0].split(": ")
+            if (tl == 3):
+                tyear = ""
+                for nc in range (1,len(ns[1])-1):
+                    tyear += ns[1][nc]
+                cyear = int(tyear)       
+            elif (tl > 4):
+                tyear = ""
+                for nc in range (1,len(ns[1])-1):
+                    tyear += ns[1][nc]
+                try:
+                    if (int(tyear) > cyear):
+                        pyear = cyear
+                        cyear = int(tyear)
+                except:
+                    tl = tl
             if (x[0] == '{"Report ID:": "------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"'):
                 yi.append(tl)
+                cyearArr.append(pyear)
                 tl = 0
             else:
                 tl += 1
@@ -59,91 +84,126 @@ with open(file_path, mode ='r') as file:
 with open(file_path, mode ='r') as file:   
         
        # reading the CSV file
-       csvFile = csv.DictReader(file)
+       csvFile = csv.reader(file)
  
        # displaying the contents of the CSV file
        y = []
        l = 0
        c = 0
+       fc = 0
+       ind = 0
+       read = 0
        for lines in csvFile:
             #print(lines)
             line = json.dumps(lines)
             x = line.rstrip().split(",")
-            #print(x)
-            if (x[0] == '{"Report ID:": "===================================================================================================================================================================================="'):
-                 l = 1
+            #print(x[0])
+            if (x[0] == '["===================================================================================================================================================================================="'):
+                if (fc != 0):
+                    l = 1
+                else:
+                    fc = 1
             elif (l > 0):
                 y.append(x)
-                print(y[c])
+                #print(y[c])
                 c += 1
                 l += 1
                 if (l == 2):
+                    if(c > 2):
+                        print("")
                     for c1 in range (0,5):
-                        ns = x[c1].split(": ")
+                        ns = x[c1]
                         if (c1 == 0):
                             surname = ""
-                            for nc in range (1, len(ns[1])):
-                                surname = surname + ns[1][nc]
-                                print(ns[1][nc],end="")
+                            for nc in range (2, len(ns)):
+                                surname = surname + ns[nc]
+                                print(ns[nc],end="")
                             print(",",end="")
                         if (c1 == 1):
                             name = ""
-                            for nc in range (0, len(ns[0])-4):
-                                name = name + ns[0][nc]
-                                print(ns[0][nc],end="")
+                            for nc in range (0, len(ns)-4):
+                                name = name + ns[nc]
+                                print(ns[nc],end="")
                             print(",",end="")
                             sex = ""
-                            for nc in range (len(ns[0])-3,len(ns[0])-1):
-                                sex = sex + ns[0][nc]
-                                print(ns[0][nc],end="")
+                            for nc in range (len(ns)-3,len(ns)-1):
+                                sex = sex + ns[nc]
+                                print(ns[nc],end="")
                             print(",",end="")
                         if (c1 == 2):
                             stdno = ""
-                            for nc in range (1, len(ns[1])-1):
-                                stdno = stdno + ns[1][nc]
-                                print(ns[1][nc],end="")
+                            for nc in range (2, len(ns)-1):
+                                stdno = stdno + ns[nc]
+                                print(ns[nc],end="")
                             print(",",end="")
                         if (c1 == 3):
                             emplID = ""
-                            for nc in range (1, len(ns[1])-1):
-                                emplID = emplID + ns[1][nc]
-                                print(ns[1][nc],end="")
+                            for nc in range (2, len(ns)-1):
+                                emplID = emplID + ns[nc]
+                                print(ns[nc],end="")
                             print(",",end="")
                         if (c1 == 4):
                             prog = ""
-                            for nc in range (1, len(ns[1])-1):
-                                prog = prog + ns[1][nc]
-                                print(ns[1][nc],end="")
+                            for nc in range (2, len(ns)-1):
+                                prog = prog + ns[nc]
+                                print(ns[nc],end="")
                             print(",",end="")
                     print("")
                     std = student(stdno,name,surname,sex,emplID,prog)
                     stdarr.append(std)
-                if (l == 4):
-                    l = 0
+                elif (l > 3):
                     for c1 in range (0,5):
-                        ns = x[c1].split(": ")
+                        ns = x[c1]
+                        ns1 = x[1]
+                        #print(ns1)
                         if (c1 == 0):
-                            for nc in range (1, len(ns[1])-1):
-                                print(ns[1][nc],end="")
-                            print(",",end="")
-                        if (c1 == 2):
-                            for nc in range (1, len(ns[1])-1):
-                                print(ns[1][nc],end="")
-                            print(",",end="")
-                        if (c1 == 3):
-                            for nc in range (1, len(ns[1])-1):
-                                print(ns[1][nc],end="")
-                            print(",",end="")
-                        if (c1 == 4):
-                            for nc in range (1, len(ns[1])-2):
-                                print(ns[1][nc],end="")
-                            print(",",end="")
-                    print("")
-                    #std = student()
-
-print(stdarr[1].stdno)
-for i in range(0,len(yi)):
-    print(yi[i],end="")
-    print(",",end="")
+                            tyear = ""
+                            for nc in range (2,len(ns)-1):
+                                tyear += ns[nc]
+                            try:
+                                cyear = int(tyear)
+                                #print(cyearArr[ind],cyear)
+                                if (cyear == cyearArr[ind]):
+                                    read = 1
+                                    ns = x[4]
+                                    sgpa = ""
+                                    for nc in range (1, len(ns)-1):
+                                        sgpa = sgpa + ns[nc]
+                                    igpa = int(sgpa)
+                                elif(cyear > cyearArr[ind]):
+                                    read = 0
+                                    ind += 1
+                            except:
+                                l = l
+                        if (c1 == 1 and read == 1 and len(ns1) == 11):
+                            nsp = x[0] 
+                            if (nsp == '[""'):
+                                courseC = ""
+                                for nc in range (2, len(ns)-1):
+                                    courseC = courseC + ns[nc]
+                                    print(ns[nc],end="")
+                                print(",",end="")
+                        if (c1 == 2 and read == 1 and len(ns1) == 11):
+                            nsp = x[0] 
+                            if (nsp == '[""'):
+                                mark = ""
+                                for nc in range (2, len(ns)-1):
+                                    mark = mark + ns[nc]
+                                    print(ns[nc],end="")
+                                print(",",end="")
+                        if (c1 == 9 and read == 1 and len(ns1) == 11):
+                            nsp = x[0]
+                            if (nsp == '[""'):
+                                courseC2 = ""
+                                for nc in range (1, len(ns[1])-1):
+                                    courseC2 = courseC2 + ns[nc]
+                                    print(ns[nc],end="")
+                                print(",",end="")
+                        
 print("")
+# print(stdarr[1].stdno)
+# for i in range(0,len(yi)):
+#     print(yi[i],"",cyearArr[i],end="")
+#     print(",",end="")
+# print("")
 
